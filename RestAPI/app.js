32261b75598aeadaaa31223c5094157cdb164352
j-nary <dlwls0921@naver.com>
2022-11-11 16:37:55 +0900

    rest api 커넥팅 완료

--- RestAPI/app.js
+++ RestAPI/app.js
@@ -5,9 +5,19 @@ var port = 5921;
 
 var port = app.listen(5921);
 
+// var link;
+// const fs = require('fs')
+// fs.readFile('/RestAPI/mongoPW.txt','utf8', (err, d) => {
+//     if (err) {
+//         console.errror(err)
+//         return
+//     }
+//     return link = d;
+// })
+// mongoose.connect(link);
 mongoose.connect('');
 var db = mongoose.connection;
-db.once('open', function() {
+db.once('open', function(err,a) {
     console.log('DB connected');
 });
 db.on('error', function(err) {
@@ -15,13 +25,21 @@ db.on('error', function(err) {
 });
 
 var contactSchema = mongoose.Schema( {
+    link:{type:String, required:true, unique:true},
     name:{type:String, required:true, unique:true},
+    star:{type:String, required:true, unique:false},
+    locate:{type:String, required:true, unique:true},
+    menus:{type:Array, required:true, unique:true}
+
 });
-var Contact = mongoose.model('contact', contactSchema);
+var Contact = mongoose.model('food', contactSchema);
 
 app.get('/contacts', (req, res) => {
-    //res.json(contacts:contacts);
-})
+    Contact.find({}, function(err, foods) {
+        if(err) return res.json(err);
+        res.json({food:foods});
+    });
+});
 
 app.listen(port, () => {
     console.log('start! express server');
